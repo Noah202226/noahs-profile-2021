b86ff39e72b8b48b1a7d171570b9204918dc603b
noah <noaligpitan26@gmail.com>
2024-04-02 20:19:18 +0800

    make navbar fixed

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import PropTypes from 'prop-types';
 
 import { Link } from "react-scroll";
 
@@ -10,6 +11,7 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 
 import MenuItem from "@mui/material/MenuItem";
 
@@ -26,7 +28,7 @@ const pages = [
 ];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
-export default function Header() {
+export default function Header(props) {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [navbarBackground, setnavbarBackground] = useState(false);
 
@@ -51,9 +53,34 @@ export default function Header() {
     document.addEventListener("scroll", changeBackgroundColor);
   }, []);
 
+  function ElevationScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+      target: window ? window() : undefined,
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+  
+  ElevationScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+  };
   return (
+    <ElevationScroll {...props}>
     <AppBar
-      position="sticky"
+      position="fixed"
       elevation={0}
       sx={{ opacity: 0.9, background: "transparent" }}
       className={navbarBackground ? "navbar" : ""}
@@ -200,5 +227,6 @@ export default function Header() {
         </Toolbar>
       </Container>
     </AppBar>
+    </ElevationScroll>
   );
 }
